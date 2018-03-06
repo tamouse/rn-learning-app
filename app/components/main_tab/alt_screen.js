90110bb1ebc6dcc0d4261cfa793e4627d427c3dd
Tamara Temple <tamara.temple@reachlocal.com>
2018-03-06 02:42:17 -0600

    remove extra comments not applicable now

--- app/components/main_tab/alt_screen.js
+++ app/components/main_tab/alt_screen.js
@@ -3,14 +3,6 @@ import { Text, View } from "react-native"
 import styles from "../../styles"
 
 export default class MainAltScreen extends React.Component {
-  /**
-   * This method of trying to keep the title centered isn't really viable -- it requires putting something like this in
-   * every component of the stack.
-   */
-  // static navigationOptions = {
-  //   headerRight: <Text>*</Text>
-  // }
-  //
   render() {
     return (
       <View style={styles.screen}>
